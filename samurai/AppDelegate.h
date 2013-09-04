6e9786fea5376db3f4ece30f6ac09a534db43a53
relsa <shinryo.u93@gmail.com>
2013-09-04 18:48:27 +0900

    First commit

---
+++ samurai/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  samurai
+//
+//  Created by CA2015 on 13/09/04.
+//  Copyright gogo-samurai 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ samurai/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  samurai
+//
+//  Created by CA2015 on 13/09/04.
+//  Copyright gogo-samurai 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
