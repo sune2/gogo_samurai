def937011bde1dd983dbb1bfaa7e8b7c6589fcf7
relsa <shinryo.u93@gmail.com>
2013-09-05 17:18:49 +0900

    add BackgroundLayer and WorkLayer

---
+++ samurai/class/GameStage/BackgroundLayer.mm
@@ -0,0 +1,13 @@
+//
+//  BackgroundLayer.m
+//  samurai
+//
+//  Created by CA2015 on 13/09/05.
+//  Copyright (c) 2013年 gogo-samurai. All rights reserved.
+//
+
+#import "BackgroundLayer.h"
+
+@implementation BackgroundLayer
+
+@end
---
+++ samurai/class/GameStage/WorkLayer.h
@@ -0,0 +1,13 @@
+//
+//  WorkLayer.h
+//  samurai
+//
+//  Created by CA2015 on 13/09/05.
+//  Copyright (c) 2013年 gogo-samurai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WorkLayer : NSObject
+
+@end
