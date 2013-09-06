802b792138afbaf84a8adfbee567070eaad81210
relsa <shinryo.u93@gmail.com>
2013-09-06 13:14:54 +0900

    add Menu.mm and Ninja.mm

---
+++ samurai/class/GameStage/Menu.h
@@ -0,0 +1,20 @@
+//
+//  Menu.h
+//  samurai
+//
+//  Created by CA2015 on 13/09/06.
+//  Copyright (c) 2013年 gogo-samurai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GLES-Render.h"
+#import "cocos2d.h"
+#import "Box2D.h"
+#import "Define.h"
+
+@interface Menu : CCLayer
+{
+    
+}
+
+@end
