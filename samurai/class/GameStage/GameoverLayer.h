ef2b2c091025f27402831cc7d8f958922c05170a
relsa <shinryo.u93@gmail.com>
2013-09-09 19:50:27 +0900

    GameoverLayerを追加

---
+++ samurai/class/GameStage/GameoverLayer.h
@@ -0,0 +1,15 @@
+//
+//  GameoverLayer.h
+//  samurai
+//
+//  Created by CA2015 on 13/09/09.
+//  Copyright (c) 2013年 gogo-samurai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "Box2D.h"
+
+@interface GameoverLayer : NSObject
+
+@end
